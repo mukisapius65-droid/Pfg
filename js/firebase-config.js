4f5c336388b6fd95938f8e76c3d93d9eb6cefcf0
iPius <mukisapius65@gmail.com>
2026-02-01 18:06:51 +0300

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration object with necessary keys.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,10 @@
-
+// PASTE YOUR ENTIRE CONFIG HERE:
+export const firebaseConfig = {
+  apiKey: "AIzaSyAGyOqUe-t04O2M6cU6Iqg8jhhhhMV4V8S_a_mk",
+  authDomain: "pfg-chapati.firebaseapp.com",
+  projectId: "pfg-chapati",
+  storageBucket: "pfg-chapati.firebasestorage.app",
+  messagingSenderId: "90335966880",
+  appId: "1:90335966880:web:3f0c4e65336b40eb25",
+  measurementId: "G-241GDRBG8N"
+};
