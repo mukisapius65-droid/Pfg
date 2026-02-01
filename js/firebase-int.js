28fd2a8edeabee86560f896dd76062873752ef87
iPius <mukisapius65@gmail.com>
2026-02-01 18:14:33 +0300

    Update and rename firebase int.js to firebase-int.js

---
+++ js/firebase-int.js
@@ -0,0 +1,9 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
